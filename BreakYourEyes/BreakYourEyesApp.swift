37301619b8c9f3112760c340a53068390e906719
Karan Upamanyu <karan.upamanyu@lyearn.com>
2024-10-05 12:45:36 +0530

    Initial Commit

---
+++ BreakYourEyes/BreakYourEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakYourEyesApp.swift
+//  BreakYourEyes
+//
+//  Created by Karan Upamanyu on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BreakYourEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakYourEyesTests/BreakYourEyesTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreakYourEyesTests.swift
+//  BreakYourEyesTests
+//
+//  Created by Karan Upamanyu on 05/10/24.
+//
+
+import Testing
+@testable import BreakYourEyes
+
+struct BreakYourEyesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
